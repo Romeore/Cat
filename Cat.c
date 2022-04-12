7583f48b3438ee69f2c6e3a5dc69430459efbd01
Alon Vaisgur <alonlonv10@gmail.com>
2022-04-12 20:47:17 +0300

    Added loops and conditions

--- Cat.c
+++ Cat.c
@@ -1,13 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#define COMMAND_NOT_FOUND -1
+#define COMMAND_NOT_FOUND   -1
 #define TURING_MACHINE_SIZE 100
-#define SYMBOL 1
+#define SYMBOL              1
+#define END_LOOP_COMMAND    7
+#define END_IF_COMMAND      9
 
 #include <string.h>
 #include "AlonQueues.h"
 
-char* commands[] = { "MEOW", "meow", "Meow", "meoW", "MeoW", "mEow"};
+char* commands[] = { "MEOW", "meow", "Meow", "meoW", "MeoW", "mEow", "MEow", "meOW", "MeOw","mEoW"};
+char turingMachine[TURING_MACHINE_SIZE];
+char* memoryLocation;
+char copyRegister;
+
+void end_program(char* message) {
+	printf("\nError - %s\n", message);
+	exit(1);
+}
 
 void searchForCommand(char* buffer, int* commandNumber) {
 
@@ -29,17 +39,21 @@ void searchForCommand(char* buffer, int* commandNumber) {
 	else if (!strcmp(buffer, commands[5])) {
 		*commandNumber = 5;
 	}
+	else if (!strcmp(buffer, commands[6])) {
+		*commandNumber = 6;
+	}
+	else if (!strcmp(buffer, commands[7])) {
+		*commandNumber = 7;
+	}
+	else if (!strcmp(buffer, commands[8])) {
+		*commandNumber = 8;
+	}
+	else if (!strcmp(buffer, commands[9])) {
+		*commandNumber = 9;
+	}
 }
 
 void executeCommands(Queue* commandQueuePtr) {
-
-	char turingMachine[TURING_MACHINE_SIZE];
-	char* memoryLocation;
-	char copyRegister;
-	
-	memoryLocation = turingMachine + TURING_MACHINE_SIZE / 2;
-	copyRegister = '0';
-	memset(turingMachine, '0', TURING_MACHINE_SIZE);
 
 	while (!isQueueEmpty(commandQueuePtr)) {
 		switch (removeQueue(commandQueuePtr)) {
@@ -57,6 +71,7 @@ void executeCommands(Queue* commandQueuePtr) {
 				break;
 			case 4:
 				printf("%c", *memoryLocation);
+				break;
 			case 5:
 				if (*memoryLocation != '0') {
 					copyRegister = *memoryLocation;
@@ -65,6 +80,63 @@ void executeCommands(Queue* commandQueuePtr) {
 					*memoryLocation = copyRegister;
 					copyRegister = '0';
 				}
+				break;
+			case 6:
+			{
+				BOOLEAN foundLoopEndCommand = FALSE;
+				Queue tempQueue;
+				Queue oneLoopQueue;
+				initQueue(&tempQueue);
+				initQueue(&oneLoopQueue);
+				char timesToExecuteRegister = *memoryLocation;
+				while (!isQueueEmpty(commandQueuePtr) && !foundLoopEndCommand) {
+					int currentCommand = removeQueue(commandQueuePtr);
+					if (currentCommand != END_LOOP_COMMAND) {
+						insertQueue(&oneLoopQueue, currentCommand);
+					}
+					else {
+						foundLoopEndCommand = TRUE;
+						while (timesToExecuteRegister-- > '0')
+						{
+							copyQueue(&oneLoopQueue, &tempQueue);
+							executeCommands(&tempQueue);
+						}
+					}
+				}
+
+				if (!foundLoopEndCommand) {
+					end_program("Missing finish loop instruction");
+				}
+			}
+			break;
+			case 7:
+				end_program("Missing start loop instruction");
+
+			case 8:
+			{
+				BOOLEAN foundEndIfCommand = FALSE;
+				Queue tempQueue;
+				initQueue(&tempQueue);
+				while (!isQueueEmpty(commandQueuePtr) && !foundEndIfCommand) {
+					int currentCommand = removeQueue(commandQueuePtr);
+					if (currentCommand != END_IF_COMMAND) {
+						insertQueue(&tempQueue, currentCommand);
+					}
+					else {
+						foundEndIfCommand = TRUE;
+						if (*memoryLocation > copyRegister) {
+							executeCommands(&tempQueue);
+						}
+					}
+				}
+
+				if (!foundEndIfCommand) {
+					end_program("Missing finish if instruction");
+				}
+				break;
+			}
+			case 9:
+				end_program("Missing start if instruction");
 		}
 	}
 }
@@ -80,6 +152,10 @@ int main(int argc, char* argv[]) {
 	file = fopen(argv[1], "rb");
 	currentCommand = COMMAND_NOT_FOUND;
 	buffer[4] = '\0';
+
+	memoryLocation = turingMachine + TURING_MACHINE_SIZE / 2;
+	copyRegister = '0';
+	memset(turingMachine, '0', TURING_MACHINE_SIZE);
 
 	if (file) {
 		while (fread(&currentSymbol, sizeof(char), SYMBOL, file) == SYMBOL) {
